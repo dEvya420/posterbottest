1b9bfebf840bafd50279bd99018956da4977eb0f
Ashu <sudhanshukumar@sggscc.ac.in>
2018-08-26 10:33:52 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,20 +4,6 @@
 
     $bot_token = "658690075:AAFQNAqF3MnR-YzLK7wx3xMFxQYwgqNhEmI";
 
-	
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	
-	
-	$dbhost     ="localhost";  
-
-	$dbname     ="XXXbot";
-
-	$dbusername ="root";
-
-	$dbpassword ="ImHere";
-
-
-
 	// Using the TelegramBotPHP library by Eleirbag89 - https://github.com/Eleirbag89/TelegramBotPHP
 
 	require ("Telegram.php");
