1bfd7bd191e9d9ffcc67e58f655a8e21d73a80fa
dEvya420 <42708777+dEvya420@users.noreply.github.com>
2018-08-26 08:53:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,16 @@ if ($text == '/start') {
     $telegram->sendMessage($content);
 }
 
+$option = array( 
+    //First row
+    array($telegram->buildInlineKeyBoardButton("Button 1", $url="http://link1.com"), $telegram->buildInlineKeyBoardButton("Button 2", $url="http://link2.com")), 
+    //Second row 
+    array($telegram->buildInlineKeyBoardButton("Button 3", $url="http://link3.com"), $telegram->buildInlineKeyBoardButton("Button 4", $url="http://link4.com"), $telegram->buildInlineKeyBoardButton("Button 5", $url="http://link5.com")), 
+    //Third row
+    array($telegram->buildInlineKeyBoardButton("Button 6", $url="http://link6.com")) );
+$keyb = $telegram->buildInlineKeyBoard($option);
+$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
+$telegram->sendMessage($content);
 
 
 ?>
